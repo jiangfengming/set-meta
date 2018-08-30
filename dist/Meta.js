f36b9c15dca7277d7d16156de0dcaa4384f9524d
Jiang Fengming <jiangfengming87@gmail.com>
2018-08-30 16:29:02 +0800

    update set-open-graph; update rollup config;

--- dist/Meta.js
+++ dist/Meta.js
@@ -22,168 +22,143 @@
     return _extends.apply(this, arguments);
   }
 
-  var commonjsGlobal = typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
+  var trimLastPart = ['og:image:url', 'og:video:url', 'og:audio:url', 'og:locale:current', 'music:album:url', 'music:song:url', 'video:actor:url'];
 
-  function unwrapExports (x) {
-  	return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, 'default') ? x['default'] : x;
-  }
-
-  function createCommonjsModule(fn, module) {
-  	return module = { exports: {} }, fn(module, module.exports), module.exports;
-  }
-
-  var OpenGraph = createCommonjsModule(function (module, exports) {
-  (function (global, factory) {
-    {
-      factory(module, exports);
-    }
-  })(commonjsGlobal, function (module, exports) {
-
-    exports.__esModule = true;
-
-    function _classCallCheck(instance, Constructor) {
-      if (!(instance instanceof Constructor)) {
-        throw new TypeError("Cannot call a class as a function");
-      }
+  var OpenGraph =
+  /*#__PURE__*/
+  function () {
+    function OpenGraph(properties, customNS) {
+      this.properties = properties;
+      this.customNS = customNS;
     }
 
-    var trimLastPart = ['og:image:url', 'og:video:url', 'og:audio:url', 'og:locale:current', 'music:album:url', 'music:song:url', 'video:actor:url'];
+    var _proto = OpenGraph.prototype;
 
-    var OpenGraph = function () {
-      function OpenGraph(properties, customNS) {
-        _classCallCheck(this, OpenGraph);
+    _proto.set = function set(properties, customNS) {
+      this.clear();
+      var ns = ['og: http://ogp.me/ns#'];
+      if (properties.fb) ns.push('fb: http://ogp.me/ns/fb#');
+      var type = properties.og && properties.og.type;
 
-        this.properties = properties;
-        this.customNS = customNS;
+      if (type && !type.includes(':')) {
+        type = type.split('.')[0];
+        ns.push(type + ": http://ogp.me/ns/" + type + "#");
       }
 
-      OpenGraph.prototype.set = function set(properties, customNS) {
-        this.clear();
+      if (customNS) ns = ns.concat(customNS);else if (this.customNS) ns = ns.concat(this.customNS);
+      document.head.setAttribute('prefix', ns.join(' '));
+      var meta = this.parse(properties);
 
-        var ns = ['og: http://ogp.me/ns#'];
-        if (properties.fb) ns.push('fb: http://ogp.me/ns/fb#');
+      if (this.properties) {
+        var exists = meta.map(function (m) {
+          return m.property;
+        });
+        var defaultMeta = this.parse(this.properties).filter(function (m) {
+          return !exists.includes(m.property);
+        });
+        if (defaultMeta.length) meta = meta.concat(defaultMeta);
+      }
 
-        var type = properties.og && properties.og.type;
-        if (type && !type.includes(':')) {
-          type = type.split('.')[0];
-          ns.push(type + ': http://ogp.me/ns/' + type + '#');
+      for (var _iterator = meta, _isArray = Array.isArray(_iterator), _i = 0, _iterator = _isArray ? _iterator : _iterator[Symbol.iterator]();;) {
+        var _ref;
+
+        if (_isArray) {
+          if (_i >= _iterator.length) break;
+          _ref = _iterator[_i++];
+        } else {
+          _i = _iterator.next();
+          if (_i.done) break;
+          _ref = _i.value;
         }
 
-        if (customNS) ns = ns.concat(customNS);else if (this.customNS) ns = ns.concat(this.customNS);
+        var m = _ref;
+        this.insertElem(m);
+      }
+    };
 
-        document.head.setAttribute('prefix', ns.join(' '));
+    _proto.clear = function clear() {
+      document.head.removeAttribute('prefix');
+      var els = document.head.querySelectorAll('meta[property]');
 
-        var meta = this.parse(properties);
+      for (var _iterator2 = els, _isArray2 = Array.isArray(_iterator2), _i2 = 0, _iterator2 = _isArray2 ? _iterator2 : _iterator2[Symbol.iterator]();;) {
+        var _ref2;
 
-        if (this.properties) {
-          var exists = meta.map(function (m) {
-            return m.property;
-          });
-          var defaultMeta = this.parse(this.properties).filter(function (m) {
-            return !exists.includes(m.property);
-          });
-          if (defaultMeta.length) meta = meta.concat(defaultMeta);
+        if (_isArray2) {
+          if (_i2 >= _iterator2.length) break;
+          _ref2 = _iterator2[_i2++];
+        } else {
+          _i2 = _iterator2.next();
+          if (_i2.done) break;
+          _ref2 = _i2.value;
         }
 
-        for (var _iterator = meta, _isArray = Array.isArray(_iterator), _i = 0, _iterator = _isArray ? _iterator : _iterator[Symbol.iterator]();;) {
-          var _ref;
+        var el = _ref2;
+        document.head.removeChild(el);
+      }
+    };
 
-          if (_isArray) {
-            if (_i >= _iterator.length) break;
-            _ref = _iterator[_i++];
-          } else {
-            _i = _iterator.next();
-            if (_i.done) break;
-            _ref = _i.value;
-          }
+    _proto.parse = function parse(obj, prefix) {
+      if (prefix === void 0) {
+        prefix = '';
+      }
 
-          var m = _ref;
-          this.insertElem(m);
-        }
-      };
+      var result = [];
 
-      OpenGraph.prototype.clear = function clear() {
-        document.head.removeAttribute('prefix');
-        var els = document.head.querySelectorAll('meta[property]');
-        for (var _iterator2 = els, _isArray2 = Array.isArray(_iterator2), _i2 = 0, _iterator2 = _isArray2 ? _iterator2 : _iterator2[Symbol.iterator]();;) {
-          var _ref2;
+      for (var k in obj) {
+        var v = obj[k];
+        if (!v) continue;
+        var property = prefix ? prefix + ':' + k : k;
+        if (trimLastPart.includes(property)) property = prefix;
 
-          if (_isArray2) {
-            if (_i2 >= _iterator2.length) break;
-            _ref2 = _iterator2[_i2++];
-          } else {
-            _i2 = _iterator2.next();
-            if (_i2.done) break;
-            _ref2 = _i2.value;
-          }
+        if (v.constructor === Object) {
+          result = result.concat(this.parse(v, property));
+        } else if (v.constructor === Array) {
+          for (var _iterator3 = v, _isArray3 = Array.isArray(_iterator3), _i3 = 0, _iterator3 = _isArray3 ? _iterator3 : _iterator3[Symbol.iterator]();;) {
+            var _ref3;
 
-          var el = _ref2;
-          document.head.removeChild(el);
-        }
-      };
-
-      OpenGraph.prototype.parse = function parse(obj) {
-        var prefix = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : '';
-
-        var result = [];
-
-        for (var k in obj) {
-          var v = obj[k];
-          if (!v) continue;
-
-          var property = prefix ? prefix + ':' + k : k;
-          if (trimLastPart.includes(property)) property = prefix;
-
-          if (v.constructor === Object) {
-            result = result.concat(this.parse(v, property));
-          } else if (v.constructor === Array) {
-            for (var _iterator3 = v, _isArray3 = Array.isArray(_iterator3), _i3 = 0, _iterator3 = _isArray3 ? _iterator3 : _iterator3[Symbol.iterator]();;) {
-              var _ref3;
-
-              if (_isArray3) {
-                if (_i3 >= _iterator3.length) break;
-                _ref3 = _iterator3[_i3++];
-              } else {
-                _i3 = _iterator3.next();
-                if (_i3.done) break;
-                _ref3 = _i3.value;
-              }
-
-              var item = _ref3;
-
-              if (item.constructor === Object) {
-                result = result.concat(this.parse(item, property));
-              } else {
-                result.push({ property: property, content: item });
-              }
+            if (_isArray3) {
+              if (_i3 >= _iterator3.length) break;
+              _ref3 = _iterator3[_i3++];
+            } else {
+              _i3 = _iterator3.next();
+              if (_i3.done) break;
+              _ref3 = _i3.value;
             }
-          } else {
-            result.push({ property: property, content: v });
+
+            var item = _ref3;
+
+            if (item.constructor === Object) {
+              result = result.concat(this.parse(item, property));
+            } else {
+              result.push({
+                property: property,
+                content: item
+              });
+            }
           }
+        } else {
+          result.push({
+            property: property,
+            content: v
+          });
         }
+      }
 
-        return result;
-      };
+      return result;
+    };
 
-      OpenGraph.prototype.insertElem = function insertElem(attrs) {
-        var meta = document.createElement('meta');
+    _proto.insertElem = function insertElem(attrs) {
+      var meta = document.createElement('meta');
 
-        for (var name in attrs) {
-          meta.setAttribute(name, attrs[name]);
-        }
+      for (var name in attrs) {
+        meta.setAttribute(name, attrs[name]);
+      }
 
-        document.head.appendChild(meta);
-      };
+      document.head.appendChild(meta);
+    };
 
-      return OpenGraph;
-    }();
-
-    exports.default = OpenGraph;
-    module.exports = exports['default'];
-  });
-  });
-
-  var OpenGraph$1 = unwrapExports(OpenGraph);
+    return OpenGraph;
+  }();
 
   var Meta =
   /*#__PURE__*/
@@ -194,7 +169,7 @@
       }
 
       this.titleTemplate = defaults.titleTemplate;
-      this.openGraph = new OpenGraph$1(defaults.openGraph, defaults.customNS);
+      this.openGraph = new OpenGraph(defaults.openGraph, defaults.customNS);
     }
 
     var _proto = Meta.prototype;
--- rollup.config.esm.mjs
+++ rollup.config.esm.mjs
@@ -1,6 +1,5 @@
 import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
 
 export default {
   input: 'src/index.mjs',
@@ -10,7 +9,6 @@ export default {
   },
   plugins: [
     resolve(),
-    commonjs(),
     babel({ exclude: 'node_modules/**' })
   ]
 }
